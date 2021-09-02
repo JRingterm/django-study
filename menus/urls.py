aadcd939487e5dd9a67dff74d9f2be56ef8e24f0
JRingterm <ghwns3712@naver.com>
2021-09-03 00:22:43 +0900

    add simple Template

---
+++ menus/urls.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from django.urls import path
+from . import views #.은 현재 같은 디렉토리를 의미. 같은 디렉토리에서 views.py를 가져와라
+
+urlpatterns = [
+    path('', views.index_view) #foods 앱 안의 views.py파일에서 index 함수를 가져와라
+]
