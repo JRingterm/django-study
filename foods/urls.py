f262041b21f29458082bbfbb121c0350dec5cacc
JRingterm <ghwns3712@naver.com>
2021-09-04 21:30:28 +0900

    study template Language and block and URL

--- foods/urls.py
+++ foods/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views #.은 현재 같은 디렉토리를 의미. 같은 디렉토리에서 views.py를 가져와라
 
 urlpatterns = [
-    path('index/', views.index) #foods 앱 안의 views.py파일에서 index 함수를 가져와라
+    path('index/', views.index), #foods 앱 안의 views.py파일에서 index 함수를 가져와라
+    path('menu/<str:food>/', views.food_detail)
 ]
